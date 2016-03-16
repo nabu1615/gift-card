909a2038b13000ac8ee96f0ad04344e4060c11a9
Javier Vargas <javvarga@cobogpgp1085.global.publicisgroupe.net>
2016-03-15 21:52:21 -0500

    header styling for  mobile and desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 $(function() {
-    var pull = $('.icon-hamburger');
-    menu = $('.main-nav');
+    var pull = $('.small-menu');
+    menu = $('.nav');
     menuHeight = menu.height();
 
    $(pull).on('click', function(e) {
     e.preventDefault();
-    menu.fadeToggle();
-    $('body').toggleClass('hidden-scroll');
+    menu.slideToggle();
 });
 
 $(window).resize(function(){
@@ -18,49 +17,5 @@ $(window).resize(function(){
     });
 });
 
-$(function(){
-    rockola = $(".btn-rockola"),
-    sPlayer = $(".player-list");
-    sHide = $(".play-hide");
-
-    $(rockola).on("click", function(e){
-        e.preventDefault();
-        $(this).parents().find(sPlayer).fadeToggle();
-    });
-});
-
-$(function(){
-    $(sHide).on("click", function(e){
-        e.preventDefault();
-        $(this).parents().find(sPlayer).fadeToggle();
-    });
-});
 
 
-(function() {
-    var script = document.createElement("script");
-
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "//sd.toneden.io/production/v2/toneden.loader.js"
-
-    var entry = document.getElementsByTagName("script")[0];
-    entry.parentNode.insertBefore(script, entry);
-}());
-
-ToneDenReady = window.ToneDenReady || [];
-ToneDenReady.push(function() {
-    ToneDen.configure({
-        soundcloudConsumerKey: '81b30d1e0691564e9128e5257eb09954'
-    });
-    // This is where all the action happens:
-    ToneDen.player.create({
-        dom: "#player",
-        eq: "waves",
-        skin: "dark",
-        tracksPerArtist: 20,
-        urls: [
-            "https://soundcloud.com/el-bandido-bistro"
-        ]
-    });
-});
